84be329f5eea80da2ce22674fffe897bfc00d762
shahrishabh7 <75043888+shahrishabh7@users.noreply.github.com>
2020-12-22 00:28:02 -0600

    Calculates distance between two cities

--- reader.py
+++ reader.py
@@ -2,23 +2,20 @@ import csv
 
 class City(object):
     # constructor with default values
-    def __init__ (self, name, latitude, longitude, country):
+    def __init__ (self, name, latitude, longitude, state, country):
         self.name = name
         self.latitude = latitude
         self.longitude = longitude
+        self.state = state
         self.country = country
 
 def reader():
+    global master
     master = []
     with open('worldcities.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         next(csv_reader)
         for row in csv_reader:
-            entry = City(row[0], float(row[2]), float(row[3]), row[4])
+            entry = City(row[0], float(row[2]), float(row[3]), row[-4], row[4])
             master.append(entry)
     return master
-
-def main():
-    masterlist = reader()
-    print(masterlist)
-main()
