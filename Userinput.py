fb465b0073ca2542438269d2a78d206356b4bcd1
Viraj Parikh <virajxp1@utexas.edu>
2020-12-23 15:34:23 -0600

    Created Userinput.py that gathers cities dates and length of stay in each city and returns them as arrays

--- Userinput.py
+++ Userinput.py
@@ -1,1 +1,30 @@
 # This file is for user input
+
+def get_cities():
+    print("Enter the cities you want to vist:")
+    prompt = ("Hit enter again if you are finished\n")
+
+    line = input(prompt)
+    cities = []
+    while line:
+        cities.append(line)
+        line = input()
+
+    return cities
+
+
+def dates(cities):
+    # date needs to be in this format
+    print("Enter your prefered start date:")
+    startDate = input("Month Day, Year \nexample 'Jan 12, 2021' \n\n")
+    lengthOfStay = []
+    print("\n")
+    for city in cities:
+        lengthOfStay.append(input(("Days in " + city + ":")))
+    return startDate, lengthOfStay
+
+
+def GetUserInput():
+    cities = get_cities()
+    startDate, lengthOfStay = dates(cities)
+    return cities, startDate, lengthOfStay
